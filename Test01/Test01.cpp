ea09318c165f07ddfb56f1e1260fd44e7ec6bd98
Thiago Moreira <Thiago_boy_5@hotmail.com>
2022-09-19 20:09:25 -0300

    Initial Commit

---
+++ Test01/Test01.cpp
@@ -0,0 +1,20 @@
+
+// Aula FAESA Técnicas de programação C++
+
+#include <iostream>
+
+/*int main()
+{
+    std::cout << "Feurrado is a cool dev!\n"; 
+}
+*/
+
+int main()
+{
+    std::cout << "Insert two numbers:" << std::endl;
+    int v1 = 0, v2 = 0;
+    std::cin >> v1 >> v2;
+    std::cout << "The sum of " << v1 << " and " << v2
+        << " is " << v1 + v2 << std::endl;
+    return 0;
+}
